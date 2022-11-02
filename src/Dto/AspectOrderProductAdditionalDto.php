f3a020201ee30183277d3b3bf95a3017cb83ac17
Nick <i@httpd.cc>
2022-11-02 10:50:04 +0800

    perf: add addons price

--- src/Dto/AspectOrderProductAdditionalDto.php
+++ src/Dto/AspectOrderProductAdditionalDto.php
@@ -2,7 +2,9 @@
 
 namespace Cblink\Service\FinchAspect\Dto;
 
+use App\Common\Third\Consts\ProductConst;
 use Cblink\Service\FinchAspect\Kernel\BaseAspectDto;
+use Hyperf\Utils\Arr;
 
 /**
  * @property string $union_id
@@ -23,4 +25,18 @@ class AspectOrderProductAdditionalDto extends BaseAspectDto
         'price_type',
         'label',
     ];
+
+    // 1固定加价，2叠加价格
+    public function getTotalFee()
+    {
+        // 默认为固定加价
+        $price = (string) $this->getItem('price', 0);
+
+        // 叠加加价
+        if ($this->getItem('price_type') == 2) {
+            $price = bcmul($price, (string) $this->getItem('num', 0), 2);
+        }
+
+        return $price;
+    }
 }
