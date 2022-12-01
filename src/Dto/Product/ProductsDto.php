1dab97b034229374168262404f26f1e3417e89a6
Nick <i@httpd.cc>
2022-12-01 10:46:21 +0800

    perf: 增加meta数据

--- src/Dto/Product/ProductsDto.php
+++ src/Dto/Product/ProductsDto.php
@@ -6,11 +6,13 @@ use Cblink\Service\FinchAspect\Kernel\BaseAspectDto;
 
 /**
  * @property ProductDto[] $products 商品集合
+ * @property array $meta meta信息
  */
 class ProductsDto extends BaseAspectDto
 {
     protected $fillable = [
-        'products'
+        'products',
+        'meta',
     ];
 
     /**
